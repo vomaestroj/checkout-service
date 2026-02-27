d947a03fe26c07909a55a5bff31984167b2403e3
Joe P <76544917+vomaestroj@users.noreply.github.com>
2026-02-26 23:41:57 -0600

    Create send_llm_trace.py

---
+++ send_llm_trace.py
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+export DD_SITE='datadoghq.com'
+export DD_LLMOBS_ML_APP='quickstart'
+export DD_LLMOBS_ENABLED=1
+
+DD_LLMOBS_ENABLED=1 DD_LLMOBS_ML_APP=quickstart \
+DD_API_KEY=${DD_API_KEY} DD_SITE=${DD_SITE} \
+DD_LLMOBS_AGENTLESS_ENABLED=1 ddtrace-run python $1
