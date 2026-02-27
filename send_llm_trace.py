954f03edecc3c71c95cfd8cd8803b71243cd69cd
Joe P <76544917+vomaestroj@users.noreply.github.com>
2026-02-27 10:07:29 -0600

    Added DD_ENV

--- send_llm_trace.py
+++ send_llm_trace.py
@@ -3,6 +3,7 @@
 export DD_SITE='datadoghq.com'
 export DD_LLMOBS_ML_APP='quickstart'
 export DD_LLMOBS_ENABLED=1
+export DD_ENV='dev'
 
 DD_LLMOBS_ENABLED=1 DD_LLMOBS_ML_APP=quickstart \
 DD_API_KEY=${DD_API_KEY} DD_SITE=${DD_SITE} \
